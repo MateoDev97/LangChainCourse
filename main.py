4de34c45e760f419014ca1e48c44a011d12d6f5b
MateoDev97 <mateo972000@gmail.com>
2026-09-23 00:41:03 +0700

    Ollama tested

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
 
 load_dotenv()
 
@@ -27,13 +28,20 @@ def summarize_with_gemini(information: str) -> str:
     return response.content
 
 
+def sumirize_with_ollama(information: str) -> str:
+    prompt = PromptTemplate.from_template(SUMMARY_TEMPLATE)
+    llm = ChatOllama(temperature=0, model="gemma3:270m")
+    response = (prompt | llm).invoke({"information": information})
+    return response.content
+
+
 def main() -> None:
     information = """
     Marco Antonio Rubio (Miami, 28 de mayo de 1971) es un político y diplomático conservador estadounidense de ascendencia cubana, perteneciente al Partido Republicano. Es el secretario de Estado de los Estados Unidos desde el 21 de enero de 2025 y, como tal, el cuarto en la línea de sucesión presidencial del país, convirtiéndose en el primer político de ascendencia hispana en obtenerlo. Es también el primer político estadounidense en ser elegido para ocupar cargos públicos de forma simultánea: secretario de estado, consejero de seguridad nacional, administrador de USAID y archivista de los Estados Unidos.
     Hijo de inmigrantes cubanos, ha sido legislador de Florida, fue uno de los senadores en el Congreso de los Estados Unidos con raíces cubanas. Rubio fue precandidato del Partido Republicano para las elecciones presidenciales de Estados Unidos de 2016.
     """
 
-    print(summarize_with_gemini(information))
+    print(sumirize_with_ollama(information))
 
 
 if __name__ == "__main__":
